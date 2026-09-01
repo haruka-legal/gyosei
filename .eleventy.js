e6170676ed0a4d10a9264a708ffc75459c9457c5
Haruka K <haruka.legal.support@gmail.com>
2026-09-01 20:29:19 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.ignores.add("src/visa/_includes/**");
+eleventyConfig.ignores.add("src/visa/**");
   // 静的ファイルはそのままコピー
 eleventyConfig.addPassthroughCopy("src/css");
 
